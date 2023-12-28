0e799b30cc830ee03d3b750454d368bc7a9c2096
Ken Harmon <41084577+harmkenn@users.noreply.github.com>
2023-12-28 14:35:35 +0000

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import a_quant, b_discrete, c_normal, d_proportions, e_studentt, f_allttests, g_chisquare, h_linearregression # import your app modules here
+from apps import a_quant, b_discrete, c_normal, d_proportions, e_studentt, f_allttests, g_chisquare, h_linearregression, i_anova # import your app modules here
 st.set_page_config(layout="wide")
 app = MultiApp()
 
@@ -13,6 +13,7 @@ app.add_app("Student T", e_studentt.app)
 app.add_app("All T-Tests", f_allttests.app)
 app.add_app("Chi-Square", g_chisquare.app)
 app.add_app("Linear Regression", h_linearregression.app)
+app.add_app("ANOVA", i_anova.app)
 
 # The main app
 app.run()
